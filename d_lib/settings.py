579bfadbbdb6dcfff5d393c17db79fd0bb00fcc4
aasis21 <ak.ashish2108@gmail.com>
2017-05-21 10:08:44 +0530

    added django-filter app

--- d_lib/settings.py
+++ d_lib/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'mylib',
     'bootstrap_admin',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
